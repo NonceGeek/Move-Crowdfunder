8e990b7228471e06d6a3abf38007021782abfe18
v1xingyue <qixingyue@gmail.com>
2023-06-04 01:38:13 +0800

    config 类型 null

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
-export const SUI_PACKAGE = process.env.NEXT_PUBLIC_DAPP_PACKAGE
-export const SUI_MODULE = process.env.NEXT_PUBLIC_DAPP_MODULE
-export const SHARE_FUND_INFO = process.env.NEXT_PUBLIC_SHARE_FUND_INFO
-export const GLOBAL_FLOW_CONFIG = process.env.NEXT_PUBLIC_FLOW_CONFIG;
-export const NETWORK = process.env.NEXT_PUBLIC_SUI_NETWORK
+export const SUI_PACKAGE = process.env.NEXT_PUBLIC_DAPP_PACKAGE!
+export const SUI_MODULE = process.env.NEXT_PUBLIC_DAPP_MODULE!
+export const SHARE_FUND_INFO = process.env.NEXT_PUBLIC_SHARE_FUND_INFO!
+export const GLOBAL_FLOW_CONFIG = process.env.NEXT_PUBLIC_FLOW_CONFIG!
+export const NETWORK = process.env.NEXT_PUBLIC_SUI_NETWORK!
 
 console.log(SHARE_FUND_INFO);
